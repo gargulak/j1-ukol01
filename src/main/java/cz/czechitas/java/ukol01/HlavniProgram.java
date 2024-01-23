117090e7be36f81553791d81961bfd73df839554
gargulak <juraj.gargulak@pobox.sk>
2024-01-23 15:37:25 +0100

    Initial commit

---
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -0,0 +1,17 @@
+package cz.czechitas.java.ukol01;
+
+import cz.czechitas.java.ukol01.engine.Turtle;
+
+public class HlavniProgram {
+
+  public static void main(String[] args) {
+    new HlavniProgram().start();
+  }
+
+  public void start() {
+    Turtle zofka = new Turtle();
+
+    //TODO implementace domácího úkolu
+  }
+
+}
